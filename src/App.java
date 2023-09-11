271dbe9a8b31acb9aed28e78ebc4a09e4afeda12
Te22E-Mrdjen-Damjan <damjan.mrdjen@elev.ga.ntig.se>
2023-09-11 09:33:38 +0200

    Gjort uppgift a)

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+      int tal1=7;
+      int tal2=10;
+
+      int kvot1=tal1/tal2;
+      double kvot2=(double)tal1/tal2;
+    System.out.println("kvoten är"+kvot1);
+    System.out.println("kvoten är"+kvot2);
     }
 }
