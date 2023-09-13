cfa359fb244e6aae5178b92b2fe2273b5b1d046d
Te22E-Mrdjen-Damjan <damjan.mrdjen@elev.ga.ntig.se>
2023-09-13 10:17:06 +0200

    uppgift 1 b)

--- src/App.java
+++ src/App.java
@@ -1,11 +1,21 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
       int tal1=7;
       int tal2=10;
 
+
+
       int kvot1=tal1/tal2;
       double kvot2=(double)tal1/tal2;
     System.out.println("kvoten är"+kvot1);
     System.out.println("kvoten är"+kvot2);
+
+    Scanner fortnite=new Scanner(System.in);
+    System.out.println("Ange ett substantiv");
+    String substantiv=fortnite.nextLine();
+    System.out.println("Ange pluralhändelse");
+    String plural=fortnite.nextLine();
+    System.out.println("En " +substantiv+ " flera " +substantiv+plural);
     }
 }
